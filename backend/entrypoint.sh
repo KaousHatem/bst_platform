b67721710ed628e590cabec2a17a7ad21922e1e3
hatem <kaous.nadir.hatem@gmail.com>
2022-03-29 12:23:02 +0100

    add dev setup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,5 +15,13 @@ fi
 python manage.py makemigrations
 python manage.py migrate
 
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 
 exec "$@"
