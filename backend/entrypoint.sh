5867278c1470cf8be7ec24dcfbacbdf4db1549e5
hatem <kaous.nadir.hatem@gmail.com>
2022-03-30 10:55:13 +0100

    fix entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -18,7 +18,7 @@ python manage.py migrate
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
     python manage.py createsuperuser \
-        --noinput \
+        --no-input \
         --username $DJANGO_SUPERUSER_USERNAME \
         --email $DJANGO_SUPERUSER_EMAIL
 fi
