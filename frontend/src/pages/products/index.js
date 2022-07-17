723efdde3ea1954c8c103dce809f4f87464b6ee4
hatem <kaous.nadir.hatem@gmail.com>
2022-07-17 09:28:41 +0100

    rename file

--- frontend/src/pages/products/index.js
+++ frontend/src/pages/products/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head';
 import React from 'react';
-import { Box, Container } from '@mui/material';
+import { 
+  Box, 
+  Container,
+  Backdrop,
+  CircularProgress,
+  Snackbar,
+  Alert,
+} from '@mui/material';
 import { ProductListResults } from '../../components/product/product-list-results';
 import { CustomerListToolbar } from '../../components/product/customer-list-toolbar';
 import { DashboardLayout } from '../../components/dashboard-layout';
@@ -9,7 +16,7 @@ import { customers } from '../../__mocks__/customers';
 import ProductProvider from '../../services/product-provider'
 import CategoryProvider from '../../services/category-provider'
 
-
+import {CONNECTION_ERROR} from '../../utils/constants'
 
 class Products extends React.Component{
 
@@ -19,7 +26,10 @@ class Products extends React.Component{
     this.state = {
       products: [],
       categories: [],
-      loading: true
+      loading: true,
+      errorSBOpen: false,
+      loadingOpen: true,
+      errorSBText: ""
     };
 
   
@@ -27,17 +37,41 @@ class Products extends React.Component{
   }
 
   componentDidMount(){
+    this.setState({loadingOpen:true,...this.state})
     Promise.all([
       ProductProvider.getProducts(),
       CategoryProvider.getCategories()
       ]).then((responses) => {
-        this.setState({products : responses[0].data,categories : responses[1].data, loading: false})
+        console.log('log')
+        this.setState({loadingOpen:false, products : responses[0].data,categories : responses[1].data, loading: false})
+      },
+      (errors)=>{
+        console.log('lof')
+        this.setState({loadingOpen:false})
+        this.handleSBOpen(CONNECTION_ERROR)
       })
     
   }
+
+  handleSBClose(){
+
+    this.setState({errorSBOpen:false,...this.state})
+  }
+
+  handleSBOpen(text){
+    this.setState({errorSBOpen:true, errorSBText:text})
+
+  }
  
   render() {
-    return(this.state.loading == false &&
+    return(<>{this.state.loading === true &&
+      <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={this.state.loadingOpen}
+          onClick={()=>{setLoadingOpen(false)}}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop> ||
     <>
       <Head>
         <title>
@@ -60,6 +94,15 @@ class Products extends React.Component{
         </Container>
       </Box>
     </>
+  }
+  <Snackbar open={this.state.errorSBOpen} 
+    onClose={()=>this.handleSBClose()}>
+      <Alert variant="filled" 
+      severity="error">
+        {this.state.errorSBText}
+      </Alert>
+    </Snackbar>
+  </>
   )
   }
     
