3c0ed1b71c72e966ea401004ffc511ab9303b89c
hatem <kaous.nadir.hatem@gmail.com>
2022-07-17 15:40:31 +0100

    fix loading users issue

--- frontend/src/pages/user/index.js
+++ frontend/src/pages/user/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head';
 import React from 'react';
-import { Box, Container, Grid, Pagination } from '@mui/material';
+import { 
+  Box, 
+  Container, 
+  Grid, 
+  Pagination,
+  Backdrop,
+  CircularProgress,
+  Snackbar,
+  Alert,
+} from '@mui/material';
  
 import { DashboardLayout } from '../../components/dashboard-layout';
 
@@ -17,24 +26,47 @@ class Users extends React.Component{
     this.state = {
       users: [],
       nextPageUrl: '',
-      loading: true
+      loading: true,
+      errorSBOpen: false,
+      loadingOpen: true,
+      errorSBText: ""
     };
 
   }
 
   componentDidMount(){
-
+    this.setState({loadingOpen:true,...this.state})
     UserProvider.getUsers().then(
       response => {
         console.log(response.data)
-        this.setState({users : response.data, loading: false})
+        this.setState({loadingOpen:false, users : response.data, loading: false})
+      },error => {
+        his.setState({loadingOpen:false})
+        this.handleSBOpen(CONNECTION_ERROR)
       }
       )
     
   }
 
+  handleSBClose(){
+    this.setState({errorSBOpen:false})
+  }
+
+  handleSBOpen(text){
+    this.setState({errorSBOpen:true, errorSBText:text})
+
+  }
+
   render() {
-    return( this.state.loading == false &&
+    return(
+    <> { this.state.loading === true &&
+      <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={this.state.loadingOpen}
+          onClick={()=>{setLoadingOpen(false)}}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop> ||
       <>
         <Head>
           <title>
@@ -55,7 +87,16 @@ class Users extends React.Component{
           
         </Box>
       </>
-      );
+      }
+      <Snackbar open={this.state.errorSBOpen} 
+      onClose={()=>this.handleSBClose()}>
+        <Alert variant="filled" 
+        severity="error">
+          {this.state.errorSBText}
+        </Alert>
+      </Snackbar>
+      
+      </>);
   }
   
 }
