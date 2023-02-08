f86b286c3184033b67203cb3ef016fe05f9ec48a
hatem <kaous.nadir.hatem@gmail.com>
2023-02-08 14:53:01 +0100

    fix search product not found

--- frontend/src/components/provision/product-add-dialog.js
+++ frontend/src/components/provision/product-add-dialog.js
@@ -30,21 +30,21 @@ import { getInitials } from '../../utils/get-initials';
 import { Positive as PositiveIcon } from '../../icons/positive';
 
 
-import {ThreeDots as ThreeDotsIcon} from '../../icons/three-dots'
-import {Edit as EditIcon} from '../../icons/edit'
-import {Delete as DeleteIcon} from '../../icons/delete'
+import { ThreeDots as ThreeDotsIcon } from '../../icons/three-dots'
+import { Edit as EditIcon } from '../../icons/edit'
+import { Delete as DeleteIcon } from '../../icons/delete'
 import Label from '../Label';
 
 import ProductProvider from '../../services/product-provider'
 
 
-export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProducts, setSelectedProducts}) => {
+export const ProductAddDialog = ({ open, handleClickOpen, setOpen, selectedProducts, setSelectedProducts }) => {
 
   const [products, setProducts] = useState([])
   const [searched, setSearched] = useState('')
   const [allProducts, setAllProducts] = useState([])
 
-  const [selectedProductIds, setSelectedProductIds] = useState(selectedProducts.map((row => {return(row.data.id)})));
+  const [selectedProductIds, setSelectedProductIds] = useState(selectedProducts.map((row => { return (row.data.id) })));
 
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
@@ -65,22 +65,24 @@ export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProdu
   };
 
   const handleAddProductToProvision = () => {
-    
 
-    setSelectedProducts([...selectedProducts,...selectedProductIds.filter((i) => {return !selectedProducts.map((product)=>{
-      return product.data.id
-    }).includes(i)}).map(i => {
-      
-        const product_i = allProducts.filter((row) => {
-          return(row.id === i)
-        })[0]
-        const product_data = {
-          data: product_i,
-          quantity: 10,
-          unit: product_i.base_unit
-        }
-        return product_data
-      } 
+
+    setSelectedProducts([...selectedProducts, ...selectedProductIds.filter((i) => {
+      return !selectedProducts.map((product) => {
+        return product.data.id
+      }).includes(i)
+    }).map(i => {
+
+      const product_i = allProducts.filter((row) => {
+        return (row.id === i)
+      })[0]
+      const product_data = {
+        data: product_i,
+        quantity: 10,
+        unit: product_i.base_unit
+      }
+      return product_data
+    }
     )])
     setLimit(10)
     setPage(0)
@@ -109,36 +111,38 @@ export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProdu
   const handleChange = (e) => {
     setSearched(e.target.value)
     const searchedProducts = allProducts.filter((row) => {
-      return(row.name.toLowerCase().includes(e.target.value.toLowerCase()) || row.sku.toLowerCase().includes(e.target.value.toLowerCase())) 
+      return (row.name.toLowerCase().includes(e.target.value.toLowerCase()) || row.sku.toLowerCase().includes(e.target.value.toLowerCase()))
     })
-      setProducts(searchedProducts)
-    if (searchedProducts.length == 0){
+    setProducts(searchedProducts)
+    if (searchedProducts.length == 0) {
       setNotFound(true)
+    } else {
+      setNotFound(false)
     }
     console.log(notFound)
-    
+
   }
 
   useEffect(() => {
-    if (allProducts.length === 0 && products.length === 0){
+    if (allProducts.length === 0 && products.length === 0) {
       ProductProvider.getProducts().then(
         (response) => {
           setAllProducts(response.data)
           setProducts(response.data)
         }
       )
-     }
+    }
   })
 
-  useEffect(()=>{
-    setSelectedProductIds(selectedProducts.map((row => {return(row.data.id)})))
+  useEffect(() => {
+    setSelectedProductIds(selectedProducts.map((row => { return (row.data.id) })))
   }, [selectedProducts])
 
-  return(
-    <Dialog open={open} 
-    onClose={handleClose}>
+  return (
+    <Dialog open={open}
+      onClose={handleClose}>
       <DialogTitle>AJOUTER ARTICLE</DialogTitle>
-      <DialogContent>   
+      <DialogContent>
         <TextField
           autoFocus
           margin="dense"
@@ -152,7 +156,7 @@ export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProdu
         />
         <Table
           sx={{
-            my:3
+            my: 3
           }}
         >
           <TableHead
@@ -174,12 +178,12 @@ export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProdu
             </TableRow>
           </TableHead>
           <TableBody>
-            {products.slice(page*limit, page*limit+limit).map((product) => (
+            {products.slice(page * limit, page * limit + limit).map((product) => (
               <TableRow
                 hover
                 key={product.id}
                 selected={selectedProductIds.indexOf(product.id) !== -1}
-                
+
               >
                 <TableCell padding="checkbox">
                   <Checkbox
@@ -196,31 +200,31 @@ export const ProductAddDialog = ({open,  handleClickOpen, setOpen, selectedProdu
                 </TableCell>
               </TableRow>
             ))}
-             {notFound && 
-                <TableRow>
-                  <TableCell 
-                  colSpan={3} 
+            {notFound &&
+              <TableRow>
+                <TableCell
+                  colSpan={3}
                   align="center">
-                    L&apos;article n&apos;existe pas
-                  </TableCell>
-                </TableRow>
-              }
+                  L&apos;article n&apos;existe pas
+                </TableCell>
+              </TableRow>
+            }
           </TableBody>
         </Table>
         <TablePagination
-        component="div"
-        count={products.length}
-        onPageChange={handlePageChange}
-        page={page}
-        rowsPerPage={limit}
-        rowsPerPageOptions={-1}
-      />
+          component="div"
+          count={products.length}
+          onPageChange={handlePageChange}
+          page={page}
+          rowsPerPage={limit}
+          rowsPerPageOptions={-1}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Annuler</Button>
         <Button onClick={handleAddProductToProvision}>Ajouter</Button>
       </DialogActions>
     </Dialog>
-    )
+  )
 
 }
