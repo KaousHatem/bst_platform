3b0e5d08f67bd6b4d26826a151887b9378af790e
hatem <kaous.nadir.hatem@gmail.com>
2023-02-12 09:51:14 +0100

    remove comment

--- frontend/src/components/provision/pdf/footer-provision.js
+++ frontend/src/components/provision/pdf/footer-provision.js
@@ -1,98 +1,91 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import {Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import { Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import ItemsTable from './items-table'
 import bstLogo from '../../../../public/static/bst-logo.jpg'
 import { format } from 'date-fns'
 
 
 const styles = StyleSheet.create({
-   
-    footerContainer:{
-        display:'flex',
+
+    footerContainer: {
+        display: 'flex',
         flexDirection: 'row',
         justifyContent: "space-between",
-        // marginTop: 'auto',
-        // marginBottom: 128,
     },
-    text:{
+    text: {
         color: 'black',
         letterSpacing: 0.5,
         fontFamily: 'Helvetica-Bold',
-        fontSize:11,
-        marginTop:5,
+        fontSize: 11,
+        marginTop: 5,
     },
 
     visaDemand: {
         display: 'flex',
-        // width: '70%',
 
     },
     VisaLogistic: {
-        marginLeft:'auto',
-        // width: '30%',
+        marginLeft: 'auto',
     },
-    chop:{
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        alignSelf:'flex-start',
-        // height: 128,
-        // width: '90',
-        // padding: 15,
+    chop: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'flex-start',
         paddingHorizontal: 5,
-        paddingTop:5,
-        border:'3 solid #04006e',
+        paddingTop: 5,
+        border: '3 solid #04006e',
         borderRadius: 10,
     },
-    qrCode:{
+    qrCode: {
         height: 75,
         width: 75,
     },
-    logo:{
-        height:30,
+    logo: {
+        height: 30,
         width: 30
     }
 
-    
-  });
+
+});
 
 
-const FooterProvision = ({value,provision, creator, approver}) => {
+const FooterProvision = ({ value, provision, creator, approver }) => {
 
     const getDate = (date) => {
         const [year, month, day] = date.split('-')
-        return format(new Date(date),'dd/MM/yyyy')
+        return format(new Date(date), 'dd/MM/yyyy')
     }
 
- 
-    
-    return(
-    <View style={styles.footerContainer}>
-        <View style={styles.visaDemand}>
-            <Text style={styles.text}>Demandeur: {provision.created_by.fullname}</Text>
-            <Text style={styles.text}>Date: {getDate(provision.created_on)}</Text>
-            <View style={styles.chop}>
-                <Image style={styles.qrCode} 
-                src={document.getElementById(creator).toDataURL()}/>
-                <Image style={styles.logo}
-                src={bstLogo.src}/>
+
+
+    return (
+        <View style={styles.footerContainer}>
+            <View style={styles.visaDemand}>
+                <Text style={styles.text}>Demandeur: {provision.created_by.fullname}</Text>
+                <Text style={styles.text}>Date: {getDate(provision.created_on)}</Text>
+                <View style={styles.chop}>
+                    <Image style={styles.qrCode}
+                        src={document.getElementById(creator).toDataURL()} />
+                    <Image style={styles.logo}
+                        src={bstLogo.src} />
+                </View>
+
             </View>
-            
+            <View style={styles.VisaLogistic}>
+                <Text style={styles.text}>St. Logistique</Text>
+                <Text style={styles.text}>Date: {provision.approved_on !== null && getDate(provision.approved_on)}</Text>
+                {provision.approved_by !== null && <View style={styles.chop}>
+                    <Image style={styles.qrCode}
+                        src={document.getElementById(approver).toDataURL()} />
+                    <Image style={styles.logo}
+                        src={bstLogo.src} />
+                </View>}
+            </View>
         </View>
-        <View style={styles.VisaLogistic}>
-            <Text style={styles.text}>St. Logistique</Text>
-            <Text style={styles.text}>Date: {provision.approved_on!==null && getDate(provision.approved_on)}</Text>
-            {provision.approved_by!==null && <View style={styles.chop}>
-                <Image style={styles.qrCode} 
-                src={document.getElementById(approver).toDataURL()}/>
-                <Image style={styles.logo}
-                src={bstLogo.src}/>
-            </View>}
-        </View>
-    </View>
-  );
+    );
 
 }
-  
-  export default FooterProvision
+
+export default FooterProvision
