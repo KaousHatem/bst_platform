742d77052042c6e4f419fc20690f216b15514e3d
hatem <kaous.nadir.hatem@gmail.com>
2023-02-13 10:46:05 +0100

    add address field

--- frontend/src/pages/project/location/add-location.js
+++ frontend/src/pages/project/location/add-location.js
@@ -5,28 +5,28 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import {  
-  Select, 
-  MenuItem, 
-  Box, 
-  Button, 
-  Container, 
-  Grid, 
-  Link, 
-  TextField, 
-  Typography, 
-  Card, 
+import {
+  Select,
+  MenuItem,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  Card,
   CardContent,
   Backdrop,
   CircularProgress,
   Snackbar,
-  Alert, 
+  Alert,
 } from '@mui/material';
-import {LocalizationProvider, DatePicker, AdapterDateFns} from '@mui/lab'
+import { LocalizationProvider, DatePicker, AdapterDateFns } from '@mui/lab'
 import InputLabel from '@mui/material/InputLabel';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-import {LocationAddToolbar} from '../../../components/project/location/location-add-toolbar'
+import { LocationAddToolbar } from '../../../components/project/location/location-add-toolbar'
 import { DashboardLayout } from '../../../components/dashboard-layout';
 import frLocale from 'date-fns/locale/fr';
 
@@ -44,7 +44,7 @@ const AddLocation = () => {
   const [loadingOpen, setLoadingOpen] = useState(false)
   const [errorSBText, setErrorSBText] = useState("")
 
-  
+
   const CONNECTION_ERROR = "Probleme de connexion, Veuillez de ressayer"
 
 
@@ -53,10 +53,11 @@ const AddLocation = () => {
   const handleOnSubmit = (e) => {
     e.preventDefault();
     const data = {
-      'name':e.target.name.value,
-      'city':e.target.city.value,
-      'state':e.target.state.value,
-      'codePostal':e.target.codePostal.value
+      'name': e.target.name.value,
+      'address': e.target.address.value,
+      'city': e.target.city.value,
+      'state': e.target.state.value,
+      'codePostal': e.target.codePostal.value
     }
 
     setLoadingOpen(true)
@@ -70,7 +71,7 @@ const AddLocation = () => {
         setLoadingOpen(false)
         handleSBOpen(CONNECTION_ERROR)
       }
-      )
+    )
   }
 
   const handleSBClose = () => {
@@ -81,110 +82,122 @@ const AddLocation = () => {
     setErrorSBText(text)
     setErrorSBOpen(true)
   }
-  
+
   return (
     <>
-    <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={loadingOpen}
-      onClick={()=>{setLoadingOpen(false)}}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-    <Head>
-      <title>
-        EURL BST | AJOUTER UN SITE
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <LocationAddToolbar/>
-        <Box sx={{ 
-          mt: 3 ,
-          backgroundColor: "white",
-        }}>
-          <Card>
-            <CardContent>
-              <Box >
-                <form id="add-location-form" 
-                onSubmit={(event) => handleOnSubmit(event)}>
-                  <Grid container 
-                  spacing={1} 
-                  columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item 
-                    xs={6}>
-                      <TextField
-                        fullWidth
-                        label="Nom du Site"
-                        margin="normal"
-                        name="name"
-                        type="text"
-                        variant="outlined"
-                        required
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <TextField
-                        fullWidth
-                        label="Ville"
-                        margin="normal"
-                        name="city"
-                        type="text"
-                        variant="outlined"
-                        required
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <TextField
-                        fullWidth
-                        label="Wilaya"
-                        margin="normal"
-                        name="state"
-                        type="text"
-                        variant="outlined"
-                        required
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <TextField
-                        fullWidth
-                        label="Code Postale"
-                        margin="normal"
-                        name="codePostal"
-                        type="text"
-                        variant="outlined"
-                        required
-                      />
-                    </Grid>
-                    
-                  </Grid>
-                </form>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loadingOpen}
+        onClick={() => { setLoadingOpen(false) }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Head>
+        <title>
+          EURL BST | AJOUTER UN SITE
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth={false}>
+          <LocationAddToolbar />
+          <Box sx={{
+            mt: 3,
+            backgroundColor: "white",
+          }}>
+            <Card>
+              <CardContent>
+                <Box >
+                  <form id="add-location-form"
+                    onSubmit={(event) => handleOnSubmit(event)}>
+                    <Grid container
+                      spacing={1}
+                      columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                      <Grid item
+                        xs={6}>
+                        <TextField
+                          fullWidth
+                          label="Nom du Site"
+                          margin="normal"
+                          name="name"
+                          type="text"
+                          variant="outlined"
+                          required
+                        />
+                      </Grid>
+                      <Grid item
+                        xs={6}>
+                        <TextField
+                          fullWidth
+                          label="Ville"
+                          margin="normal"
+                          name="city"
+                          type="text"
+                          variant="outlined"
+                          required
+                        />
+                      </Grid>
+                      <Grid item
+                        xs={6}>
+                        <TextField
+                          fullWidth
+                          label="Wilaya"
+                          margin="normal"
+                          name="state"
+                          type="text"
+                          variant="outlined"
+                          required
+                        />
+                      </Grid>
+                      <Grid item
+                        xs={6}>
+                        <TextField
+                          fullWidth
+                          label="Code Postale"
+                          margin="normal"
+                          name="codePostal"
+                          type="text"
+                          variant="outlined"
+                          required
+                        />
+                      </Grid>
+                      <Grid item
+                        xs={12}>
+                        <TextField
+                          fullWidth
+                          label="Adresse"
+                          margin="normal"
+                          name="address"
+                          type="text"
+                          variant="outlined"
+                          required
+                        />
+                      </Grid>
 
-                
-              </Box>
-            </CardContent>
-          </Card>
-        </Box>
-      </Container>
-      <Snackbar open={errorSBOpen} 
-      onClose={handleSBClose}>
-        <Alert variant="filled" 
-        severity="error">
-          {errorSBText}
-        </Alert>
-      </Snackbar>
-    </Box>
+                    </Grid>
+                  </form>
 
-  </>
+
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+        </Container>
+        <Snackbar open={errorSBOpen}
+          onClose={handleSBClose}>
+          <Alert variant="filled"
+            severity="error">
+            {errorSBText}
+          </Alert>
+        </Snackbar>
+      </Box>
+
+    </>
   );
 };
 
--- frontend/src/pages/project/location/edit-location.js
+++ frontend/src/pages/project/location/edit-location.js
@@ -5,28 +5,28 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import {  
-  Select, 
-  MenuItem, 
-  Box, 
-  Button, 
-  Container, 
-  Grid, 
-  Link, 
-  TextField, 
+import {
+  Select,
+  MenuItem,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
   Typography,
-  Card, 
-  CardContent, 
+  Card,
+  CardContent,
   Backdrop,
   Snackbar,
   Alert,
   CircularProgress,
 } from '@mui/material';
-import {LocalizationProvider, DatePicker, AdapterDateFns} from '@mui/lab'
+import { LocalizationProvider, DatePicker, AdapterDateFns } from '@mui/lab'
 import InputLabel from '@mui/material/InputLabel';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-import {LocationAddToolbar} from '../../../components/project/location/location-add-toolbar'
+import { LocationAddToolbar } from '../../../components/project/location/location-add-toolbar'
 import { DashboardLayout } from '../../../components/dashboard-layout';
 import frLocale from 'date-fns/locale/fr';
 
@@ -37,13 +37,6 @@ import LocationProvider from '../../../services/location-provider'
 
 
 const EditLocation = () => {
-
-  const [value, setValue] = useState(new Date());
-  const [locationValue, setLocationValue] = useState("");
-  const [locations, setLocations] = useState([])
-  const [roleValue, setRoleValue] = useState("");
-
-
 
   const router = useRouter();
   const [locationId, setLocationId] = useState(router.query.id);
@@ -61,14 +54,15 @@ const EditLocation = () => {
   const handleOnSubmit = (e) => {
     e.preventDefault();
     const data = {
-      'name':e.target.name.value,
-      'city':e.target.city.value,
-      'state':e.target.state.value,
-      'codePostal':e.target.codePostal.value
+      'name': e.target.name.value,
+      'address': e.target.address.value,
+      'city': e.target.city.value,
+      'state': e.target.state.value,
+      'codePostal': e.target.codePostal.value
     }
-    
+
     setLoadingOpen(true)
-    LocationProvider.updateLocation(data,locationId).then(
+    LocationProvider.updateLocation(data, locationId).then(
       (response) => {
         setLoadingOpen(false)
         router.push('/project/location/');
@@ -77,7 +71,7 @@ const EditLocation = () => {
         setLoadingOpen(false)
         handleSBOpen(CONNECTION_ERROR)
       }
-      )
+    )
   }
 
   const handleSBClose = () => {
@@ -92,135 +86,140 @@ const EditLocation = () => {
 
   useEffect(() => {
     setLoadingOpen(true)
-    if(locationId){
+    if (locationId) {
       LocationProvider.getLocations(locationId).then(
         (response) => {
 
           setLocation(response.data)
           SetLoading(false)
-          setLoadingOpen(false)          
+          setLoadingOpen(false)
         },
         (error) => {
           setLoadingOpen(false)
           handleSBOpen(CONNECTION_ERROR)
         }
-        )
+      )
     }
-  },[locationId])
+  }, [locationId])
 
-  
+
   return (
-     
-    <>
-    <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={loadingOpen}
-      onClick={()=>{setLoadingOpen(false)}}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-    <Head>
-      <title>
-        EURL BST | AJOUTER UN SITE
-      </title>
-    </Head>
-    {loading === false &&
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <LocationAddToolbar/>
-        <Box sx={{ 
-          mt: 3 ,
-          backgroundColor: "white",
-        }}>
-          <Card>
-            <CardContent>
-              <Box >
-                <form id="add-location-form" 
-                onSubmit={(event) => handleOnSubmit(event)}>
-                  <Grid container 
-                  spacing={1} 
-                  columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item 
-                    xs={6}>
-                      <InputLabel>
-                        Nom du Site
-                      </InputLabel>
-                      <TextField
-                        fullWidth
-                        margin="normal"
-                        name="name"
-                        type="text"
-                        variant="outlined"
-                        defaultValue={location.name}
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <InputLabel>
-                        Ville 
-                      </InputLabel>
-                      <TextField
-                        fullWidth
-                        margin="normal"
-                        name="city"
-                        type="text"
-                        variant="outlined"
-                        defaultValue={location.city}
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <InputLabel>
-                        Wilaya
-                      </InputLabel>
-                      <TextField
-                        fullWidth
-                        margin="normal"
-                        name="state"
-                        type="text"
-                        variant="outlined"
-                        defaultValue={location.state}
-                      />
-                    </Grid>
-                    <Grid item 
-                    xs={6}>
-                      <InputLabel>
-                        Code Postale
-                      </InputLabel>
-                      <TextField
-                        fullWidth
-                        margin="normal"
-                        name="codePostal"
-                        type="text"
-                        variant="outlined"
-                        defaultValue={location.codePostal}
-                      />
-                    </Grid>
-                    
-                  </Grid>
-                </form>
 
-                
-              </Box>
-            </CardContent>
-          </Card>
-        </Box>
-      </Container>
-    </Box>}
-    <Snackbar open={errorSBOpen} 
-    onClose={handleSBClose}>
-      <Alert variant="filled" 
-      severity="error">
-        {errorSBText}
-      </Alert>
-    </Snackbar>
-  </>
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loadingOpen}
+        onClick={() => { setLoadingOpen(false) }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Head>
+        <title>
+          EURL BST | EDITER UN SITE
+        </title>
+      </Head>
+      {loading === false &&
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            py: 8
+          }}
+        >
+          <Container maxWidth={false}>
+            <LocationAddToolbar />
+            <Box sx={{
+              mt: 3,
+              backgroundColor: "white",
+            }}>
+              <Card>
+                <CardContent>
+                  <Box >
+                    <form id="add-location-form"
+                      onSubmit={(event) => handleOnSubmit(event)}>
+                      <Grid container
+                        spacing={1}
+                        columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid item
+                          xs={6}>
+                          <TextField
+                            fullWidth
+                            label="Nom du Site"
+                            margin="normal"
+                            name="name"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={location.name}
+                          />
+                        </Grid>
+                        <Grid item
+                          xs={6}>
+                          <TextField
+                            fullWidth
+                            label="Ville"
+                            margin="normal"
+                            name="city"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={location.city}
+                          />
+                        </Grid>
+                        <Grid item
+                          xs={6}>
+                          <TextField
+                            fullWidth
+                            label="Wilaya"
+                            margin="normal"
+                            name="state"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={location.state}
+                          />
+                        </Grid>
+                        <Grid item
+                          xs={6}>
+                          <TextField
+                            fullWidth
+                            label="Code Postale"
+                            margin="normal"
+                            name="codePostal"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={location.codePostal}
+                          />
+                        </Grid>
+
+                        <Grid item
+                          xs={12}>
+                          <TextField
+                            fullWidth
+                            label="Adresse"
+                            margin="normal"
+                            name="address"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={location.address}
+                          />
+                        </Grid>
+
+                      </Grid>
+                    </form>
+
+
+                  </Box>
+                </CardContent>
+              </Card>
+            </Box>
+          </Container>
+        </Box>}
+      <Snackbar open={errorSBOpen}
+        onClose={handleSBClose}>
+        <Alert variant="filled"
+          severity="error">
+          {errorSBText}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
