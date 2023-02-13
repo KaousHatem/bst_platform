5262242c77b671283a8fd693320f29b0de09820a
hatem <kaous.nadir.hatem@gmail.com>
2023-02-13 06:26:14 +0100

    add fullname to localstorage

--- frontend/src/services/auth-provider.js
+++ frontend/src/services/auth-provider.js
@@ -1,30 +1,30 @@
-import {LoginUrl} from '../utils/networks'
+import { LoginUrl } from '../utils/networks'
 import axios from "axios";
 
 
 
 class AuthProvider {
 
-    
 
-    login( username, password ) {
-        const data =  { username, password }
-            
+
+    login(username, password) {
+        const data = { username, password }
+
         //const headers = new Headers({ 'Content-Type': 'application/json',})
-        
+
 
         return axios
-            .post(LoginUrl,data)
+            .post(LoginUrl, data)
             .then(response => {
-                console.log(response)
                 if (response.status < 200 || response.status >= 300) {
                     console.log(response)
                     throw new Error(response.statusText);
-                }else{
+                } else {
+                    localStorage.setItem('fullname', response.data.fullname)
                     localStorage.setItem('auth', response.data.access);
                     localStorage.setItem('role', response.data.role);
                     return response;
-                }     
+                }
             })
             .catch((error) => {
                 throw error
@@ -36,11 +36,11 @@ class AuthProvider {
         console.log(localStorage.getItem('auth'))
         return localStorage.getItem('auth');
     }
-    
-    logout(){
-    localStorage.clear()
-    // localStorage.removeItem('role')
-}
+
+    logout() {
+        localStorage.clear()
+        // localStorage.removeItem('role')
+    }
 }
 
 
