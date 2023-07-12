dedf9e357996415d3485d073b03c82c6fecf85a3
hatem <kaous.nadir.hatem@gmail.com>
2023-07-12 10:30:47 +0100

    update paresers

--- frontend/src/utils/parsers.js
+++ frontend/src/utils/parsers.js
@@ -1,9 +1,15 @@
 
 
 export const parseNumber = (number) => {
-        if (number / 10 >= 1){
-            return number
-        }else{
-            return "0" + number
-        }
+    if (number / 10 >= 1) {
+        return number
+    } else {
+        return "0" + number
     }
+}
+
+export const parseMoney = (value) =>
+    new Intl.NumberFormat('fr-CA', {
+        style: 'currency',
+        currency: 'DZD'
+    }).format(value);
